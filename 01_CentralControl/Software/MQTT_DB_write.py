8a5cf271099f079c2be07e332f0d4f2e9181812d
Peiren Yang <yangpeiren@163.com>
2018-06-09 23:09:48 +0200

    created mqtt db module

---
+++ 01_CentralControl/Software/MQTT_DB_write.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2010-2013 Roger Light <roger@atchoo.org>
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Distribution License v1.0
+# which accompanies this distribution.
+#
+# The Eclipse Distribution License is available at
+#   http://www.eclipse.org/org/documents/edl-v10.php.
+#
+# Contributors:
+#    Roger Light - initial implementation
+# Copyright (c) 2010,2011 Roger Light <roger@atchoo.org>
+# All rights reserved.
+
+# This shows a simple example of an MQTT subscriber.
+
+import context  # Ensures paho is in PYTHONPATH
+import paho.mqtt.client as mqtt
+from Web_view import db, FTS
+
+
+counter = 0
+
+def on_connect(mqttc, obj, flags, rc):
+    print("rc: " + str(rc))
+
+
+def on_message(mqttc, obj, msg):
+    global counter
+    counter += 1
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+    content = msg.payload.split('#')
+    data_item = FTS(*content)
+    db.session.add(data_item)
+    if counter == 10:
+        db.session.commit()
+
+
+def on_publish(mqttc, obj, mid):
+    print("mid: " + str(mid))
+
+
+def on_subscribe(mqttc, obj, mid, granted_qos):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+
+def on_log(mqttc, obj, level, string):
+    print(string)
+
+
+# If you want to use a specific client id, use
+# mqttc = mqtt.Client("client-id")
+# but note that the client id must be unique on the broker. Leaving the client
+# id parameter empty will generate a random id for you.
+mqttc = mqtt.Client()
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+mqttc.on_subscribe = on_subscribe
+# Uncomment to enable debug messages
+# mqttc.on_log = on_log
+# TODO: change here the server address before use
+mqttc.connect("m2m.eclipse.org", 1883, 60)
+# TODO: change here the topic before use
+mqttc.subscribe("$SYS/#", 0)
+
+db.create_all()
+
+mqttc.loop_forever()
